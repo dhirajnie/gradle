acca81512be0946226bc9be7dda0f7b995226920
dhiraj kumar <dhiraj.kumar2@microfocus.com>
2021-09-08 08:18:33 +0530

    gradle spring boot

---
+++ gradlet-demo/src/main/java/com/learning/gradletdemo/GradletDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.gradletdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradletDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradletDemoApplication.class, args);
+	}
+
+}
---
+++ gradlet-demo/src/test/java/com/learning/gradletdemo/GradletDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.gradletdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradletDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
